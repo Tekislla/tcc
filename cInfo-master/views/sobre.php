44b610190b30d51865594f510126cb7d87e81a44
João Pedro Lazarim de Souza <jplazarim@gmail.com>
2017-10-31 08:42:08 -0200

    Add files via upload

--- cInfo-master/views/sobre.php
+++ cInfo-master/views/sobre.php
@@ -5,31 +5,31 @@
 <link rel="stylesheet" href="<?php echo $config['base_url'] ?>bibliotecas/estilo/interno/sobre.css">
 
 <br>
-    <div class="ui middle aligned center aligned grid" style="margin-top: 130px">
+    <div class="ui middle aligned center aligned grid" id="div1">
         <div class="ui container">
-            <div class="ui segments" style="padding: 10px;">
-                <div class="ui two column grid ">
-                    <div class="column" style="text-align: left">
+            <div class="ui segments" id="div2">
+                <div class="ui two column grid">
+                    <div class="column" id="divLeft">
                     <p>jka asdfasdfasdf asdfasdf asd hbaasdadfasdjklfashLorem ipsum dolor sit amet, consectetur adiadsf asasdfuiasydf dfasdf asdfasdfasdf asdfasdf asdf  sdfgsdfg sdfgsdfgsdfg sdfgasjkdf piscing elit. Mauris blandit sem vitae condimentum iaculis. Sed venenatis pellentesque enim, at cursus ipsum hendrerit nec. Pellentesque commodo lacus tristique, laoreet purus at, porta leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vulputate sapien felis, tincidunt interdum purus venenatis at. Pellentesque imperdiet sem sed nisl dictum consequat. Sed consectetur mauris nibh, eu imperdiet sem mattis accumsan. Vivamus placerat felis at felis ullamcorper, sit amet pharetra mi tincidunt. Aenean sapien arcu, fringilla a euismod vel, tincisdfgjklsdfh sdfg jh jk s  sdjfghsdjkfghsdfjkg sed in egestas leo. Integer sollicitudin ex neque. Curabitur commodo luctus nunc ut sagittis. Nunc non volutpat diam. Vivamus non semper turpis. Proin venenatis mauris sit amet est imperdiet viverra.kdufa sdfagsdhfgahsdgahgjfafadgh fakdghfakdhgfakdh sgfakdhfasd hgak fgak fgasdfhg afgafgaksdfha gkdfhagsd fsdf dddfsdffsfds sdfsdfSDFASD ASDFASD kgsdasda</p>
                     </div>
-                    <div class="column" style="text-align: right">
-                        <img src="sdfsdf.jpg" width="90%"; height="30%">
+                    <div class="column" id="divRight">
+                        <img src="sdfsdf.jpg" id="img1" width="90%"; height="30%">
                     </div>
                 </div>
-                <div class="ui two column grid ">
-                    <div class="column" style="text-align: left">
-                        <img src="sdfsdf.jpg" width="90%"; height="30%">
+                <div class="ui two column grid">
+                    <div class="column" id="divLeft" style="text-align: left">
+                        <img src="sdfsdf.jpg" id="img1">
                     </div>
-                    <div class="column" style="text-align: right">
+                    <div class="column" id="divRight">
                         <p>jka asdfasdfasdf asdfasdf asd hbaasdadfasdjklfashLorem ipsum dolor sit amet, consectetur adiadsf asasdfuiasydf dfasdf asdfasdfasdf asdfasdf asdf  sdfgsdfg sdfgsdfgsdfg sdfgasjkdf piscing elit. Mauris blandit sem vitae condimentum iaculis. Sed venenatis pellentesque enim, at cursus ipsum hendrerit nec. Pellentesque commodo lacus tristique, laoreet purus at, porta leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vulputate sapien felis, tincidunt interdum purus venenatis at. Pellentesque imperdiet sem sed nisl dictum consequat. Sed consectetur mauris nibh, eu imperdiet sem mattis accumsan. Vivamus placerat felis at felis ullamcorper, sit amet pharetra mi tincidunt. Aenean sapien arcu, fringilla a euismod vel, tincisdfgjklsdfh sdfg jh jk s  sdjfghsdjkfghsdfjkg sed in egestas leo. Integer sollicitudin ex neque. Curabitur commodo luctus nunc ut sagittis. Nunc non volutpat diam. Vivamus non semper turpis. Proin venenatis mauris sit amet est imperdiet viverra.kdufa sdfagsdhfgahsdgahgjfafadgh fakdghfakdhgfakdh sgfakdhfasd hgak fgak fgasdfhg afgafgaksdfha gkdfhagsd fsdf dddfsdffsfds sdfsdfSDFASD ASDFASD kgsdasda</p>
                     </div>
                 </div>
-                <div class="ui two column grid ">
-                    <div class="column" style="text-align: left">
+                <div class="ui two column grid">
+                    <div class="column" id="divLeft">
                         <p>jka asdfasdfasdf asdfasdf asd hbaasdadfasdjklfashLorem ipsum dolor sit amet, consectetur adiadsf asasdfuiasydf dfasdf asdfasdfasdf asdfasdf asdf  sdfgsdfg sdfgsdfgsdfg sdfgasjkdf piscing elit. Mauris blandit sem vitae condimentum iaculis. Sed venenatis pellentesque enim, at cursus ipsum hendrerit nec. Pellentesque commodo lacus tristique, laoreet purus at, porta leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vulputate sapien felis, tincidunt interdum purus venenatis at. Pellentesque imperdiet sem sed nisl dictum consequat. Sed consectetur mauris nibh, eu imperdiet sem mattis accumsan. Vivamus placerat felis at felis ullamcorper, sit amet pharetra mi tincidunt. Aenean sapien arcu, fringilla a euismod vel, tincisdfgjklsdfh sdfg jh jk s  sdjfghsdjkfghsdfjkg sed in egestas leo. Integer sollicitudin ex neque. Curabitur commodo luctus nunc ut sagittis. Nunc non volutpat diam. Vivamus non semper turpis. Proin venenatis mauris sit amet est imperdiet viverra.kdufa sdfagsdhfgahsdgahgjfafadgh fakdghfakdhgfakdh sgfakdhfasd hgak fgak fgasdfhg afgafgaksdfha gkdfhagsd fsdf dddfsdffsfds sdfsdfSDFASD ASDFASD kgsdasda</p>
                     </div>
-                    <div class="column" style="text-align: right">
-                        <img src="sdfsdf.jpg" width="90%"; height="30%">
+                    <div class="column" id="divRight">
+                        <img src="sdfsdf.jpg" id="img1">
                     </div>
                 </div>
             </div>
